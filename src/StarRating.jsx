4433646013b8933a1378e1e5dd60a01eb97b3d03
Shaoyi Lin <alex.lin@verbinteractive.com>
2021-02-18 00:58:01 +0800

    Add hover function

--- src/StarRating.jsx
+++ src/StarRating.jsx
@@ -3,7 +3,7 @@ import {FaStar} from 'react-icons/fa';
 
 const StarRating = () => {
   const [rating, setRating] = useState(null);
-  
+  const [hover, setHover] = useState(null);
   return (
     <div>
       {[...Array(5)].map((star, index) => {
@@ -18,7 +18,9 @@ const StarRating = () => {
             />
             <FaStar
               size={100}
-              color={ratingValue <= rating ? '#ffc107' : '#e4e5e9'}
+              color={ratingValue <= (hover || rating) ? '#ffc107' : '#e4e5e9'}
+              onMouseEnter={() => setHover(ratingValue)}
+              onMouseLeave={() => setHover(null)}
             />
           </label>
         );
